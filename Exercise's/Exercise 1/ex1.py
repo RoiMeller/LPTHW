71c277d17e675d3f61b604ab1ad32d856e58db06
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:11:02 +0200

    Ex1: A Good First Program

---
+++ Exercise's/Exercise 1/ex1.py
@@ -0,0 +1,11 @@
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this"
+#print "this is fun..."
+#print "Yay! printing"
+#print "i'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+
+print """LPTHW Exercise 1. print another line... Yay! \n
+Exercises 2 and 3 elimnate all lines but the one I wrote by using '#' octothorpe/pound/hash/mesh... printing sucks
+"""
