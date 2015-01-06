caa705b45c33e2d63d57fe2f84ae640b31e5ed7e
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:16:13 +0200

    Ex10: What Was That?

---
+++ Exercise's/Exercise 10/ex10.py
@@ -0,0 +1,42 @@
+# LPTHW Exercise 10 - what was that? (reference to exercise 9)
+
+# This all of the escape sequences Python supports.
+
+#	\\		backslash ()
+#	\' 		Single-quote (')
+#	\" 		Double-quote (")
+#	\a 		ASCII bell (BEL)
+#	\b 		ASCII backspace (BS)
+#	\f 		ASCII formfeed (FF)
+#	\n 		ASCII linefeed (LF)
+#	\N{name} 	Character named name in the Unicode database (Unicode only)
+#	\r ASCII 	Carriage Return (CR)
+#	\t ASCII 	Horizontal Tab (TAB)
+#	\uxxxx 		Character with 16-bit hex value xxxx (Unicode only)
+#	\Uxxxxxxxx 	Character with 32-bit hex value xxxxxxxx (Unicode only)
+#	\v 		ASCII vertical tab (VT)
+#	\ooo 		Character with octal value ooo
+#	\xhh 		Character with hex value hh
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+# using ''' instead of """
+fat_cat = '''
+I'll do a list"
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# Infinite loop
+#while True:
+#	for i in ["/","-","|","\\","|"]:
+#		print "%s\r" % i,
+
