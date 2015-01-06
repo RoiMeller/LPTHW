6ddd9455acb89ef334c765510a1ce0dfcfb6cbbd
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:12:43 +0200

    Ex3: Numbers And Math

---
+++ Exercise's/Exercise 3/ex3.py
@@ -0,0 +1,47 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent 
+# < less than
+# > greater than
+# <= less-than-equal
+# >= greater-than-equal
+
+# complete the drill section by Rewrite ex3.py to use floating point numbers so it's more accurate.
+
+# printing...
+print "I will now count my chickens:"
+
+# print the sum of Hens  25 + 30 / 6 which is 30
+print "Hens", 25.0 + 30.0 / 6.0 
+# print the sum of Roosters 100 - 25 * 3 % 4 which is 97 
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# printing...
+print "Now I will count the eggs:"
+
+# prints the sum of eggs which is 6.75
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# printing < case
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# print if its false or true (false)
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# prints the sum of 3 + 2
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+# prints the sum of 5 - 7
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+
+# printing...
+print "Oh, that's why it's False."
+print "How about some more..."
+
+# check if it greather
+print "Is it greather?", 5.0 > -2.0
+# check if it greather or equal
+print "Is it greather?", 5.0 >= -2.0
+# check if it less or equal
+print "Isit less or equal?", 5.0 <= -2.0
