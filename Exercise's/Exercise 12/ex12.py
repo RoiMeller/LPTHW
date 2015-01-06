d0a1f2101958123414d209aba2586ab983dcf773
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:17:58 +0200

    Ex12: Prompting People

---
+++ Exercise's/Exercise 12/ex12.py
@@ -0,0 +1,10 @@
+# LPTHW Exercise 12 - promoting people
+
+# Rewrite exercise 11 usint just raw_input()
+age = raw_input("How old are you ? ")
+height = raw_input("How tall are you ? ")
+weight = raw_input("How much do you weight ? ")
+
+print "So, your'e %r old, %r tall and %r heavy..." % (
+	age, height, weight)
+
