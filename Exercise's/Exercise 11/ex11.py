dd994b4d9181ab4b27bcbff989e32b9ae8188d67
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:17:20 +0200

    Ex11: Asking Questions

---
+++ Exercise's/Exercise 11/ex11.py
@@ -0,0 +1,17 @@
+# LPTHW Exercise 11
+
+# We put a ',' (comma) at the end of each print line. 
+# This is so print doesn't end the line with a newline character and go to the next line.
+
+print "How old are you ?",
+age = raw_input()
+
+print "How tall are you ?",
+height = raw_input()
+
+print "How much do you weight ?",
+weight = raw_input()
+
+print "So, your'e %r old, %r tall and %r heavy..." % (
+	age, height, weight)
+
