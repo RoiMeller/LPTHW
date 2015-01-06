3d6f84952ca4fa876442228782d527167a5de82f
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:28:59 +0200

    Ex29: What If

---
+++ Exercise's/Exercise 29/ex29.py
@@ -0,0 +1,29 @@
+# LPTHW Exercise 29 - What If
+
+pepole = 20
+cats = 30
+dogs = 15
+
+if pepole < cats:
+	print "Too many cats! The world is doomed!"
+
+if pepole > cats:
+	print "Not mant cats! The world is saved!"
+
+if pepole < dogs:
+	print "Thw worls is drooled on!"
+
+if pepole > dogs:
+	print "The world is dry!"
+
+# dogs = dogs + 5
+dogs += 5
+
+if pepole >= dogs:
+	print "Pepole are greater than or equal to dogs."
+
+if pepole <= dogs:
+	print "Pepole are less than or equal to dogs."
+
+if cats != dogs and pepole == dogs:
+	print "Pepole are dogs."
