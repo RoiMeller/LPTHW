4b80556840350ded1ce55523fa83077e319be6a3
RoiMeller <Roi.meller@gmail.com>
2015-01-06 15:24:01 +0200

    Ex18: Names, Variables, Code, Functions

---
+++ Exercise's/Exercise 18/ex18.py
@@ -0,0 +1,24 @@
+# LPTHW Exercise 18 - Names, Variables, Code, Function
+
+# def (=define) will tell python we want to make a function
+# print_two = the name of the function
+# *args - the variables names ( its has to gp inside '()' parentheses
+# we end the function line with :
+def print_two(*args):
+# all the lines that are indented four spaces will become attached to this function
+	arg1, arg2 = args
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+def print_one(arg1):
+	print "arg1: %r" % arg1
+
+def print_none():
+	print "I got nothin'g..."
+
+print_two("Roi", "Meller")
+print_two_again("Roi", "Meller")
+print_one("First!")
+print_none()
